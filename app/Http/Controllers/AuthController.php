e0988ddf78bb7c6dfd04042145c770e0d3834fd7
Ekin Koseoglu <ekindenizkoseoglu@gmail.com>
2021-11-04 18:49:47 +0300

    Add (Login - Register - Logout) features in system

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,71 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request; // We are going to get request as a email-password
+use Illuminate\Http\Responses; // And we are going to give some informations as a response
+use Illuminate\Support\Facades\Hash; // We need to hash the passwords that we get from request
 
 class AuthController extends Controller
 {
+    // 
+    public function register(Request $request)
+    {
+        $fields = $request->validate([ // Take these inputs as a parameter of $fields variable
+            'name' => "required|string",
+            'email' => "required|string|unique:users,email",
+            'password' => "required|string|confirmed"
+        ]);
+        $user = User::create([ // Use these $fields parameters for creating new User (Register User) and Match them
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']) // Encrypts the password that 
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken; // Create the token for created user above
+
+        $response = [ // Give response as a user
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return [
+            'Message' => 'Logged Out From System'
+        ];
+    }
+    public function login(Request $request)
+    {
+        $fields = $request->validate([ // Take these inputs as a parameter of $fields variable
+
+            'email' => "required|string",
+            'password' => "required|string"
+        ]);
+
+        // Check Email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check Password
+        if (!$user || !Hash::check($fields['password'], $user->password)) { // If one of 2 condition is not true
+            return response([
+                'message' => "Bad creds"
+            ], 401);
+        }
+
+
+        $token = $user->createToken('myapptoken')->plainTextToken; // Create the token for created user above
+
+        $response = [ // Give response as a user
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,20 +21,23 @@ use Illuminate\Support\Facades\Route;
 // Route::apiResource('products', ProductController::class);
 
 
-// Public Routes
+// Public Routes -- Everyone going to be able to do these wtihout need a token
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 
 
 
 
-// Protected Routes
+// Protected Routes   ---  You going to need a token to be able to do these
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{id}', [ProductController::class, 'update']);
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
